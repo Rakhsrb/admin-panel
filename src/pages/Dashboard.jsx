fe1efd43fa257b6cefa01228c9866df10664599f
OMAKHMUDOFF <omaxmudovv@gmail.com>
2024-05-07 13:49:59 +0500

    login page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ export const Dashboard = () => {
   return (
     <section className="h-full bg-cyan-900">
       <div className="flex items-center justify-center h-full">
-
+        hello
       </div>
     </section>
   );
